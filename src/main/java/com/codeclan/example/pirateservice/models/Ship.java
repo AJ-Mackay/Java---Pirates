6c3236b508fd54103c3d80d13f7d48a237a0f3e8
Ruiner85 <paul.m.mackay85@gmail.com>
2020-04-06 21:40:42 +0100

    test fixed

--- src/main/java/com/codeclan/example/pirateservice/models/Ship.java
+++ src/main/java/com/codeclan/example/pirateservice/models/Ship.java
@@ -11,22 +11,20 @@ public class Ship {
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="ship_id")
+    @Column(name="id")
     private Long id;
 
-    @Column(name="name")
+    @Column(name="ship_name")
     private String name;
 
-    @JsonIgnoreProperties("ship")
-    @OneToMany(mappedBy="ship")
+//    @JsonIgnoreProperties("ship")
+    @OneToMany(mappedBy = "ship")
     private List<Pirate> pirates;
 
-    public Ship(String name){
+    public Ship(String name) {
         this.name = name;
         this.pirates = new ArrayList<Pirate>();
     }
-
-    public Ship(){}
 
     public Long getId(){
         return id;
@@ -39,7 +37,16 @@ public class Ship {
     public String getName(){
         return name;
     }
+
     public void setName(String name){
         this.name = name;
+    }
+
+    public List<Pirate> getPirates(){
+        return pirates;
+    }
+
+    public void setPirates(List<Pirate> pirates){
+        this.pirates = pirates;
     }
 }
